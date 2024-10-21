8e67fe28d55255cf1a71dab77643f4473a57ff88
Marcin Owsiany <porridge@redhat.com>
2024-10-21 12:42:45 +0200

    ROX-26600: Import 4.5.3 and add a Konflux build (#11)
    The [build used
    here](https://github.com/stackrox/stackrox/commit/818ca4e0b4ea31b59b927b9cfec149029dec2a9e)
    is just a recent build that had the fewest number of konflux job
    failures.
    
    I was able to successfully deploy stackrox using:
    ```
    $ make -C operator deploy-via-olm \
      ROX_PRODUCT_BRANDING=RHACS_BRANDING \
      INSTALL_CHANNEL=stable \
      INSTALL_VERSION=v4.6.0-719-g818ca4e0b4-fast \
      INDEX_IMG_TAG=ocp-v4-12-08d6a6347ca235323261db44a0a271d8d808cee2-fast-amd64
    $ make -C operator stackrox-image-pull-secret NAMESPACE=stackrox-operator
    ```
    And then creating a `Central` in OpenShift console, logging in,
    generating init bundle, applying it, creating a `SecuredCluster` using
    the console.
    
    I confirmed that:
    - the operator image was the one built on Konflux
    - the central `main` image was the one built on GHA 🤷🏻 
    
    Then I wiped everything, repeated the above with
    `INSTALL_VERSION=v4.5.3` and afterwards upgraded the operator to the dev
    version, and confirmed central still works and shows the upgraded
    version in UI.

---
+++ render-template.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# The version-controled catalog template in common/catalog-template.json is
+# currently auto-generated from the production catalog (in turn built by
+# CPaaS/IIB). This makes it hard to modify (because a refresh would wipe manual
+# modifications).
+#
+# This script:
+# 1. creates a temporary catalog template from the version controled template
+# by injecting a bundle for a Konflux build, together with a rhacs-4.6 channel
+# 2. renders this template into a version-controled catalog.
+#
+# TODO: Once we stop building operator indexes using CPaaS/IIB, the entries for
+# the Konflux build can be moved into the version-controled template and the
+# script can be removed in favor of the render-template command.
+
+set -euo pipefail
+
+# A recent (more or less) successful konflux build.
+version="v4.6.0-719-g818ca4e0b4-fast"
+
+tmp_template="$(mktemp)"
+trap 'rm -f $tmp_template' EXIT
+jq --slurpfile channel channel-4.6.json '.entries += $channel
+ | .entries += [{"schema": "olm.bundle", "image": "quay.io/rhacs-eng/stackrox-operator-bundle:'${version}'"}]
+ | .entries |= map(
+   if .schema == "olm.channel" and .name == "stable"
+   then .entries += [{"name": "rhacs-operator.'${version}'", "replaces": "rhacs-operator.v4.5.3", "skipRange": ">= 4.5.0 < 4.6.0"}]
+   else .
+   end)
+ | .entries |= map(
+   if .schema == "olm.channel" and .name == "rhacs-4.6"
+   then .entries += [{"name": "rhacs-operator.'${version}'", "replaces": "rhacs-operator.v4.5.0", "skipRange": ">= 4.5.0 < 4.6.0"}]
+   else .
+   end)
+' common/catalog-template.json > "$tmp_template"
+echo "Running template rendering, this can take a few minutes..."
+opm alpha render-template basic "${tmp_template}" > common/catalog/rhacs-operator/catalog.json
