40dfebcdc6de3ceafa4251395b895393f23ec1ce
Marcin Owsiany <porridge@redhat.com>
2024-10-24 22:13:10 +0200

    Bump versions, use digest for the dev bundle. (#24)
    For `oc adm catalog mirror` (needed to [perform disconnected
    install](https://issues.redhat.com/browse/ROX-26629)) to work, bundles
    must be specified by digest. This change does it. While at it:
    - import v4.5.4 now that it's released
    - automate updating of the replaced version when injecting the dev
    bundle
    - improve instructions for bumping dev bundle version
    - add a target for importing legacy catalog

--- Makefile
+++ Makefile
@@ -2,18 +2,26 @@ CATALOGS = common/catalog-bundle-object/rhacs-operator/catalog.json common/catal
 
 MAKEFLAGS += "-j 2"
 
-.PHONY: valid-catalogs clean
+.PHONY: valid-catalogs clean import-legacy
 valid-catalogs: $(CATALOGS)
 	opm validate common/catalog-bundle-object
 	opm validate common/catalog-csv-metadata
 
 clean:
 	rm -f $(CATALOGS)
+	rm -rf catalog-migrate
 
-common/catalog-bundle-object/rhacs-operator/catalog.json: common/catalog-template.json
+common/catalog-bundle-object/rhacs-operator/catalog.json: common/catalog-template.json render-template.sh
 	mkdir -p "$$(dirname "$@")"
 	./render-template.sh --migrate-level none > $@
 
-common/catalog-csv-metadata/rhacs-operator/catalog.json: common/catalog-template.json
+common/catalog-csv-metadata/rhacs-operator/catalog.json: common/catalog-template.json render-template.sh
 	mkdir -p "$$(dirname "$@")"
 	./render-template.sh --migrate-level bundle-object-to-csv-metadata > $@
+
+# This is broken due to concurrency if invoked together with other targets (e.g. `make import-legacy valid-catalogs` - don't do this).
+# Instead invoke `make import-legacy && make valid-catalogs`.
+# TODO: fix it. Otherwise this target will disappear once konflux index builds replace the CPaaS-based ones.
+import-legacy:
+	opm migrate registry.redhat.io/redhat/redhat-operator-index:v4.12 ./catalog-migrate
+	opm alpha convert-template basic ./catalog-migrate/rhacs-operator/catalog.json > common/catalog-template.json
--- render-template.sh
+++ render-template.sh
@@ -16,15 +16,28 @@
 set -euo pipefail
 
 # A recent (more or less) successful konflux build.
-version="v4.6.0-719-g818ca4e0b4-fast"
+# To find this value:
+# - look in https://github.com/stackrox/stackrox/commits/master for a commit with a successful "Red Hat Konflux / operator-bundle-build" status,
+# - go to "Details" next to it,
+# - go to "build-container" under "Task Statuses",
+# - go to the "Details" tab in the main pane,
+# - scroll down to "Results",
+# - take the tag from the "IMAGE_URL row"
+# - take the whole value from the "IMAGE_DIGEST" row,
+# - save and run make.
+version="v4.6.0-812-gf3f0d00320-fast"
+digest="sha256:bb0c4104de9bb3670f5d7ec02339c5ff35095ebae23617c29018867934cdcb69"
+
+# This
+latest_legacy_version="$(jq -r '.entries[]|select(.schema=="olm.channel" and .name == "stable") | .entries|.[-1] | .name' < common/catalog-template.json)"
 
 tmp_template="$(mktemp)"
 trap 'rm -f $tmp_template' EXIT
 jq --slurpfile channel channel-4.6.json '.entries += $channel
- | .entries += [{"schema": "olm.bundle", "image": "quay.io/rhacs-eng/stackrox-operator-bundle:'${version}'"}]
+ | .entries += [{"schema": "olm.bundle", "image": "quay.io/rhacs-eng/stackrox-operator-bundle@'${digest}'"}]
  | .entries |= map(
    if .schema == "olm.channel" and .name == "stable"
-   then .entries += [{"name": "rhacs-operator.'${version}'", "replaces": "rhacs-operator.v4.5.3", "skipRange": ">= 4.5.0 < 4.6.0"}]
+   then .entries += [{"name": "rhacs-operator.'${version}'", "replaces": "'"${latest_legacy_version}"'", "skipRange": ">= 4.5.0 < 4.6.0"}]
    else .
    end)
  | .entries |= map(
