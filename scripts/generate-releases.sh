4aab5901f290e2988dd861a7f23ded11e218ebf7
Aleksandr Kurlov <akurlov@redhat.com>
2025-07-18 11:24:56 +0200

    Fix generation script

--- scripts/generate-releases.sh
+++ scripts/generate-releases.sh
@@ -3,35 +3,7 @@ source "$(dirname "$0")/helpers.sh"
 
 set -euo pipefail
 
-if [[ "$#" -lt 1 || "$#" -gt 3 ]]; then
-    echo "USAGE: ./generate-releases.sh <ENVIRONMENT> [COMMIT] [BRANCH]" >&2
-    echo "" >&2
-    echo "ENVIRONMENT - allowed values: staging|prod" >&2
-    echo "COMMIT - a 40 character-long SHA of the commit to pull Snapshots only with this commit label for the Release. Default: currently checked out commit" >&2
-    echo "BRANCH - an optional parameter to specify git branch name for filtering snapshots by having branch name in annotations. Default: currently checked out branch" >&2
-    echo "" >&2
-    echo "You must have your KUBECONFIG point to the Konflux cluster, see https://spaces.redhat.com/pages/viewpage.action?pageId=407312060#HowtoeverythingKonfluxforRHACS-GettingocCLItoworkwithKonflux." >&2
-    exit 1
-fi
-
-ENVIRONMENT="$1"
-COMMIT="${2:-$(git rev-parse HEAD)}"
-BRANCH="${3:-$(git rev-parse --abbrev-ref HEAD)}"
-
-release_name="${ENVIRONMENT}-$(git rev-parse --short HEAD)-$(date +'%Y-%m-%d-%H-%M')"
-# Fetch the list of snapshots for COMMIT and BRANCH. 
-# Make sure that only one the most recent snapshot per application is returned.
-snapshot_list="$(kubectl -n rh-acs-tenant get snapshot.appstudio.redhat.com -l pac.test.appstudio.openshift.io/sha="${COMMIT}" -o json | jq -r '
-  .items
-  | map(select((.metadata.annotations["pac.test.appstudio.openshift.io/source-branch"]=="'"${BRANCH}"'") or (.metadata.annotations["pac.test.appstudio.openshift.io/source-branch"]=="refs/heads/'${BRANCH}'")))
-  | sort_by(.spec.application)
-  | group_by(.spec.application)
-  | map(sort_by(.metadata.creationTimestamp) | last)
-  | .[]
-  | "\(.metadata.name)|\(.spec.application)"
-')"
-
-validate_input() {
+validate_parameters() {
     pipelines_count="$(find ".tekton" -maxdepth 1 -type f -name "operator-index-ocp-*-build.yaml" | wc -l )"
     snapshots_count="$(echo "$snapshot_list" | wc -l )"
     echo -e "found the following snapshots for \033[0;32m$COMMIT\033[0m commit in \033[0;32m$BRANCH\033[0m branch:" >&2
@@ -95,7 +67,7 @@ validate_branch "$BRANCH"
 release_name="${ENVIRONMENT}-$(git rev-parse --short HEAD)-$(date +'%Y-%m-%d-%H-%M')"
 # Fetch the list of snapshots for COMMIT and BRANCH. 
 # Make sure that only one the most recent snapshot per application is returned.
-snapshot_list="$(kubectl get snapshot -l pac.test.appstudio.openshift.io/sha="${COMMIT}" -o json | jq -r '
+snapshot_list="$(kubectl get -n rh-acs-tenant snapshot -l pac.test.appstudio.openshift.io/sha="${COMMIT}" -o json | jq -r '
   .items
   | map(select((.metadata.annotations["pac.test.appstudio.openshift.io/source-branch"]=="'"${BRANCH}"'") or (.metadata.annotations["pac.test.appstudio.openshift.io/source-branch"]=="refs/heads/'${BRANCH}'")))
   | sort_by(.spec.application)
