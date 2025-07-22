6d3da6723597f24edb8d2e2c9814cfdc250462b7
Aleksandr Kurlov <akurlov@redhat.com>
2025-07-22 11:29:53 +0200

    Use snapshots instead of pipelineruns for getting built images

--- scripts/get-built-images.sh
+++ scripts/get-built-images.sh
@@ -14,9 +14,6 @@ fi
 COMMIT="${1:-$(git rev-parse HEAD)}"
 
 echo -e "Operator catalog images for commit \033[0;32m$COMMIT\033[0m:"
-kubectl -n rh-acs-tenant get pipelinerun.tekton.dev -l pipelinesascode.tekton.dev/sha="${COMMIT}" -o json | jq -r '
-    if .items | length == 0 then
-        "No PipelineRun CRs found for the current commit. It might be pruned already from the cluster. Use Konflux UI instead: https://konflux-ui.apps.stone-prd-rh01.pg1f.p1.openshiftapps.com/ns/rh-acs-tenant/applications"
-    else
-        .items[] | select(.status.conditions[]? | select(.type == "Succeeded" and .status == "True")) .status.results[]? | select(.name == "IMAGE_URL") | .value
-    end'
+result="$(kubectl get -n rh-acs-tenant snapshot -l pac.test.appstudio.openshift.io/sha="${COMMIT}" -o jsonpath='{range .items[*].spec.components[?(@.containerImage)]}{.name}: {.containerImage}{"\n"}{end}')"
+: "${result:?Error: No Snapshot CRs found for the commit. Use Konflux UI to get built images instead: https://konflux-ui.apps.stone-prd-rh01.pg1f.p1.openshiftapps.com/ns/rh-acs-tenant/applications.}"
+echo "$result"
